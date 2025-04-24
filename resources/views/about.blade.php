3f85d16e1f53f971479d135a5bd446efa95f1c60
Andrejs Nikolajevs <nikolajevs04@gmail.com>
2025-04-24 14:45:51 +0300

    5 uzd

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>About Page</h1>
+    <p>Hello, my name is {{ $name }}.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque urna eu metus pharetra, in facilisis velit sollicitudin.</p>
+    
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function () {
+//     // return view('welcome');
+
+// });
+
+Route::get('/test', function () {
+    $rickrollUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    echo '<a href="' . $rickrollUrl . '" target="_blank">Klikšķini šeit, lai redzētu kaut ko interesantu!</a>';
+});
+
+Route::get('/about', [AboutController::class, 'index']);
